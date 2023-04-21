5169d83a53a272fd63b906dac33c64cf5d09eb1f
HANK062 <joerg.hanke@bertelsmann.de>
2023-04-21 14:02:34 +0200

    wip

--- src/test/java/com/klosebros/kata/MinesweeperTest.java
+++ src/test/java/com/klosebros/kata/MinesweeperTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class KataTemplateTest {
+class MinesweeperTest {
 
     @Test
     void deleteThisTest() {
