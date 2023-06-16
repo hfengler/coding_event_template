9a05cdf6147dc36799f0bc7db5b1bb66d7567202
Marcus Kleinert <marcus.kleinert@bertelsmann.de>
2023-06-16 14:01:28 +0200

    add new test cases

--- src/test/java/com/klosebros/kata/GildedRoseTest.java
+++ src/test/java/com/klosebros/kata/GildedRoseTest.java
@@ -58,4 +58,19 @@ class GildedRoseTest {
         assertThat(app.items[0].quality).isEqualTo(50);
     }
 
+    @Test
+    void sulfurasNeverLoseQuality() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 1, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(10);
+    }
+
+    @Test
+    void backstageTicketGetHigherValueIfSellInGetsSmaller() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 5)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].quality).isEqualTo(7);
+    }
 }
