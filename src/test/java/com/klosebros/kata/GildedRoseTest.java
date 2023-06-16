9b0eef76d90258ddf8b1485ef17d3a5486f46534
Christian Gierhake <christian.gierhake@bertelsmann.de>
2023-06-16 13:41:31 +0200

    First Test

--- src/test/java/com/klosebros/kata/GildedRoseTest.java
+++ src/test/java/com/klosebros/kata/GildedRoseTest.java
@@ -7,12 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GildedRoseTest {
 
+    private final static String AGED_BRIE = "Aged Brie";
+
     @Test
     void foo() {
         Item[] items = new Item[] {new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        Assertions.assertThat(app.items[0].name).isEqualTo("fixme");
+        Assertions.assertThat(app.items[0].name).isEqualTo("foo");
+    }
+
+    @Test
+    void agedBrieGetsBetter() {
+        var app = createGildedRose(new Item(AGED_BRIE, 10, 0));
+        app.updateQuality();
+        Assertions.assertThat(app.items[0].quality).isEqualTo(1);
+    }
+
+    private GildedRose createGildedRose(Item... items) {
+        return new GildedRose(items);
     }
 
 }
