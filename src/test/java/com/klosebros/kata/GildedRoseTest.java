76febd6cd9ccb25be44fa1fb3894d5df8409965d
Emanuel Schulze Pröbsting <emanuel.schulzeproebsting@bertelsmann.de>
2023-06-16 13:55:46 +0200

    add some more awesome tests

--- src/test/java/com/klosebros/kata/GildedRoseTest.java
+++ src/test/java/com/klosebros/kata/GildedRoseTest.java
@@ -3,11 +3,11 @@ package com.klosebros.kata;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GildedRoseTest {
 
     private final static String AGED_BRIE = "Aged Brie";
+    private static final String AGED_SOCK = "Aged Sock";
+    private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
 
     @Test
     void foo() {
@@ -18,25 +18,39 @@ class GildedRoseTest {
     }
 
     @Test
-    void agedBrieGetsBetter() {
-        var app = createGildedRose(new Item(AGED_BRIE, 10, 0));
-        app.updateQuality();
-        Assertions.assertThat(app.items[0].quality).isEqualTo(1);
-    }
-    @Test
     void AgedSockGetsWorse() {
-        var app = createGildedRose(new Item("Aged Sock", 10, 1));
+        var app = createGildedRose(new Item(AGED_SOCK, 10, 1));
         app.updateQuality();
         Assertions.assertThat(app.items[0].quality).isZero();
     }
 
     @Test
-    void experiationProductsGetsMoreWorse() {
-        var app = createGildedRose(new Item("Aged Sock", 0, 2));
+    void expatriationProductsGetsMoreWorse() {
+        var app = createGildedRose(new Item(AGED_SOCK, 0, 2));
         app.updateQuality();
         Assertions.assertThat(app.items[0].quality).isZero();
     }
 
+    @Test
+    void qualityIsAlwaysPositiv() {
+        var app = createGildedRose(new Item(AGED_SOCK, 0, 0));
+        app.updateQuality();
+        Assertions.assertThat(app.items[0].quality).isZero();
+    }
+
+    @Test
+    void agedBrieIsNeverBetterThan50() {
+        var app = createGildedRose(new Item(AGED_BRIE, 10, 50));
+        app.updateQuality();
+        Assertions.assertThat(app.items[0].quality).isEqualTo(50);
+    }
+
+//    @Test
+//    void sulfurasQualityIsAllwaysSame() {
+//        var app = createGildedRose(new Item(SULFURAS_HAND_OF_RAGNAROS, 10, 50));
+//        app.updateQuality();
+//        Assertions.assertThat(app.items[0].quality).isEqualTo(50);
+//    }
 
     private GildedRose createGildedRose(Item... items) {
         return new GildedRose(items);
